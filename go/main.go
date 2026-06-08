a4dab91acd70f3e11bd7e5e07626bc0f4a0fcb80
stevenene <stevekid705@gmail.com>
2026-06-08 22:05:48 +0300

    feat: enhance server functionality with new routes and handlers chore: update .gitignore to include additional file types

--- go/main.go
+++ go/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "neneloga.local/server"
+import (
+	"net/http"
+
+	"neneloga.local/server"
+
+	// "log"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	server.Start()
+	fmt.Print("Starting server...")
+
+	r := gin.Default()
+  r.SetTrustedProxies([]string{"192.168.1.2"})
+
+	r.GET("/ping",server.Ping )
+	
+
+
+
+
+	http.HandleFunc("/",server.Home)
+	http.HandleFunc("/rait",server.FileWrite)
+
+	// 404
+	http.HandleFunc("/404",server.NotFound)
+
+
+
+	// log.Fatal(
+		// http.ListenAndServe(":8080", nil)
+		r.Run()
+		fmt.Println("server running on :8080")
+	// )
+
 }
--- go/server/server.go
+++ go/server/server.go
@@ -1,9 +1,49 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	// "os"
+	"net/http"
+	// "log"
+	"github.com/gin-gonic/gin"
+	)
 
-func Start() {
-	fmt.Print("Starting server...")
-	fmt.Println("Done!")
+
+type Input struct {
+	Name string `json:"name"`
+	Body []byte `json:"body"`
+
 }
+
+type Output struct {
+	Message string `json:"message"`
+}
+
+func Ping(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	}
+
+func FileWrite(w http.ResponseWriter,r *http.Request){
+
+}
+
+
+func Home(w http.ResponseWriter,r *http.Request){
+	fmt.Println("Home")
+	fmt.Println(r.RemoteAddr)
+}
+
+func NotFound(w http.ResponseWriter,r *http.Request){
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type","application/json")
+	fmt.Fprintln(w,`
+		<h1>404</h1>
+		<p>Oops! Page not found</p>
+	`)
+	fmt.Println("404")
+	// fmt.Println(w,r)
+}
+
 
